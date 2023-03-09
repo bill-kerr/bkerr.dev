0843407fa6f609ebe400192510975498f9a3ab41
bkerr <bk7987@gmail.com>
2023-03-09 07:12:56 -0600

    Prefetch links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -13,6 +12,9 @@ import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), mdx(), image()]
+  integrations: [tailwind(), alpinejs(), mdx(), image(), prefetch()]
 });
