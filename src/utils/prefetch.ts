2ad9eaff1e26486123b8c8ed83ac31ff6d1c0f7d
Bill Kerr <bill@tailwindapp.com>
2023-03-11 12:59:01 -0600

    Prefetch resume images

---
+++ src/utils/prefetch.ts
@@ -0,0 +1,11 @@
+export function createImagePrefetchScript(src: string) {
+  const varName = `i${Math.floor(Math.random() * 10000)}`;
+
+	return `if (!prefetched.has("${src}")){` +
+			`const ${varName}=document.createElement("link");` +
+			`${varName}.rel="prefetch";` +
+			`${varName}.href="${src}";` + 
+			`${varName}.type="image/webp";` + 
+			`$el.appendChild(${varName});` +
+			`prefetched.add("${src}");}`;
+}
