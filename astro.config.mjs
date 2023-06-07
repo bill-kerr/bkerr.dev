0dff98564f94d21c2c51c2bd107dea2b280c3c35
Bill Kerr <bill@tailwindapp.com>
2023-06-07 08:50:23 -0500

    Add update to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
 
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), prefetch()]
+	integrations: [tailwind(), alpinejs(), prefetch(), mdx()],
 });
